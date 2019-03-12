cec1a40aad514999770349c10334c20bf03196a6
juniorandrade1 <junior.andrade.cco@gmail.com>
2019-03-11 21:00:04 -0300

    Stack prototype

---
+++ codes/Stack/Stack.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace Retroactivity {
+  template<typename T>
+  class PartialStack {
+  public:
+    void Insert_Push(int t, const T &x) {
+    }
+    void Insert_Pop(int t) {
+    }
+    void Delete_Push(int t) {
+    }
+    void Delete_Pop(int t) {
+    }
+    T peek(int t) {
+    }
+  };
+
+  template<typename T>
+  class FullStack {
+  public:
+    void Insert_Push(int t, T &x) {
+    }
+    void Insert_Pop(int t) {
+    }
+    void Delete_Push(int t) {
+    }
+    void Delete_Pop(int t) {
+    }
+    T peek() {
+    }
+  };
+}
